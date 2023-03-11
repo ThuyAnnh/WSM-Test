15fc739231658ac21445037859688fe4878ed52a
ThuyAnnh <thanhthuy1065@gmail.com>
2023-03-11 12:12:03 +0700

    git add

---
+++ src/test/java/WSM_Women_Mode/Profile.java
@@ -0,0 +1,11 @@
+package WSM_Women_Mode;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Profile {
+    private String nameInfor;
+    private String infor;
+}
---
+++ src/test/java/WSM_Women_Mode/WomenMode.java
@@ -0,0 +1,141 @@
+package WSM_Women_Mode;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WomenMode {
+    private WebDriver webDriver;
+    private int i;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+        webDriver = new ChromeDriver();
+        webDriver.manage().window().maximize();
+
+        webDriver.get("https://edev.sun-asterisk.vn/en/dashboard/users/4384/request_leaves/new");
+        WebDriverWait wait = new WebDriverWait(webDriver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@class='wsm-btn btn-login']"))).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='user_email']"))).sendKeys("nguyen.thi.my.hanh@framgia.com.edev.test");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='user_password']"))).sendKeys("123456");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='wsm-login-button']"))).click();
+
+    }
+
+    @Test
+    public void TC01() throws InterruptedException {
+        webDriver.get("https://edev.sun-asterisk.vn/en/dashboard/users/4384/request_leaves/new");
+        WebDriverWait wait = new WebDriverWait(webDriver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@class='wsm-btn btn-login']"))).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='user_email']"))).sendKeys("nguyen.thi.my.hanh@framgia.com.edev.test");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='user_password']"))).sendKeys("123456");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='wsm-login-button']"))).click();
+        WebElement tiltle = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@class='panel-title text-center']")));
+        String tiltle1 = tiltle.getText();
+        Assert.assertEquals(tiltle1,"Create new other request");
+    }
+    @Test
+    public void TC02() throws InterruptedException {
+        WebDriverWait wait = new WebDriverWait(webDriver, 10);
+        WebElement tiltle = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@class='panel-title text-center']")));
+        String tiltle1 = tiltle.getText();
+        Assert.assertEquals(tiltle1,"Create new other request");
+
+        WebElement staffName = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='employee_name']")));
+        String staffName1 = staffName.getText();
+        Assert.assertEquals(staffName1,"Staff name");
+
+        WebElement staffCode = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='employee_code']")));
+        String staffCodee1 = staffName.getText();
+        Assert.assertEquals(staffName1,"Staff code");
+
+        WebElement branch = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='select2-workspace-selection-container']")));
+        String branch1 = staffName.getText();
+        Assert.assertEquals(staffName1,"Branch");
+
+        WebElement group = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='select2-group-selection-container']")));
+        String group1 = staffName.getText();
+        Assert.assertEquals(staffName1,"Group");
+
+        WebElement leaveType = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='request_leave_leave_type_id']")));
+        String leaveType1 = staffName.getText();
+        Assert.assertEquals(staffName1,"Leave Type");
+
+        WebElement requestDate = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='request_leave_request_date']")));
+        String requestDate1 = staffName.getText();
+        Assert.assertEquals(staffName1,"Request Date");
+
+        WebElement womenMode = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=-swap_type_swap_woman']")));
+        String womenMode1 = staffName.getText();
+        Assert.assertEquals(staffName1,"Women's mode");
+
+        WebElement annualLeave = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='swap_type_swap_paid']")));
+        String annualLeave1 = staffName.getText();
+        Assert.assertEquals(staffName1,"Annual leave");
+
+        WebElement unpaidLeave = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='swap_type_swap_unpaid']")));
+        String unpaidLeave1 = staffName.getText();
+        Assert.assertEquals(staffName1,"Unpaid leave");
+
+        WebElement save = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@type='submit' and @value='Save']")));
+        String save1 = staffName.getText();
+        Assert.assertEquals(staffName1,"Save");
+
+    }
+
+    @Test
+    public void TC03() throws InterruptedException {
+        WebDriverWait wait = new WebDriverWait(webDriver, 10);
+        WebElement employeeName = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='employee_name']")));
+//        Thread.sleep(10000);
+        String employeeName1 = employeeName.getText();
+        System.out.println(employeeName1);
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@href='#' and @class='dropdown-toggle avatar-circle no-background']"))).click();
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@href='/en/dashboard/users/4384']"))).click();
+//        List<WebElement> list;
+//        List<Profile> list2 = new ArrayList<>();
+//        list = webDriver.findElements(By.xpath("//*[@class='table table-hover']//tr"));
+////        Thread.sleep(10000);
+//        list2 = tableInformationUser(list);
+//        String employeeName2 = list2.get(0).getInfor();
+//        Assert.assertEquals(employeeName2,employeeName1);
+//        WebElement staffName = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='employee_name']")));
+//        String staffName1 = staffName.getText();
+//        Assert.assertEquals(staffName1,"Staff name");
+
+    }
+    @After
+    public void tearDown(){
+        webDriver.close();
+        webDriver.quit();
+    }
+    private List<Profile> tableInformationUser(List<WebElement> list){
+//        list = webDriver.findElements(By.xpath("//*[@class='table table-hover']//tr//td"));
+//        int b = list.size();
+        List<Profile> profile = new ArrayList<>();
+        for (WebElement element:list){
+            List<WebElement> list1;
+            list1 = element.findElements(By.xpath(".//td"));
+//            System.out.println(list1);
+
+            if (list1.size() == 2) {
+                String nameInfor = list1.get(0).getText();
+                String infor = list1.get(1).getText();
+
+                Profile profile1 = new Profile(nameInfor,infor);
+                profile.add(profile1);
+            }
+        }
+        return profile;
+    }
+}
